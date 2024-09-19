da228e70e5dbc11b0110bc768576b00a491aab92
MatthewSidoti <coding.sidoti@gmail.com>
2024-09-19 18:07:56 -0400

    2 down

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//Starting
+
+//1.
+
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+const multiplied = nums.map((currentElement) => {
+  return currentElement  *  2;
+});
+
+console.log(multiplied);
+
+//2.
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+const firstPizza= [pizzaToppings[0], pizzaToppings[1]];
+
+console.log(firstPizza);
+
+// Your code here
